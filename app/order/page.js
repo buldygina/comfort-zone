224a983ff706d36ff2db07fe89f7c963edb82f38
buldygina <el.buldygina@mail.ru>
2024-05-01 01:00:41 +0300

    added page "basket" and "order"

--- app/order/page.js
+++ app/order/page.js
@@ -31,6 +31,21 @@ export default function Order() {
                 <label htmlFor='name-city'>City</label>
                 <input id='name-city'/>
             </form>
+            <div className='radioOrder'>
+            <form>
+                <input type="radio" name="rb" id="rb1" checked/> <label htmlFor="rb1"><p>Delivery to CDEK warehouse from 4 days from 6,75$.</p></label>
+            </form>
+            <form>
+                <input type="radio" name="rb" id="rb2"/> <label htmlFor="rb2"><p>Delivery to the door by CDEK from 4 days from 9,22$.</p> </label>
+            </form>
+
+            <form>
+                <input type="radio" name="rb" id="rb3"/> <label for="rb3"><p>Express delivery to CDEK warehouse from 4 days from 11,66$.</p></label>
+            </form>
+            <form>
+                <input type="radio" name="rb" id="rb4"/> <label htmlFor="rb4"><p>Express delivery to the door by CDEK from 4 days from 13$.</p></label>
+            </form>
+            </div>
         </div>
     );
 }
