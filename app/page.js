8a838584d0bd5bd285db3a74e20201e9f1be97d6
buldygina <el.buldygina@mail.ru>
2024-05-15 14:15:11 +0300

    added items to menu-favourites

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { SlBasket } from "react-icons/sl";
 import { AiOutlineClose } from "react-icons/ai";
 import { IoCloseSharp } from "react-icons/io5";
+import { CgCloseO } from "react-icons/cg";
 const colors = ["grey", "grey", "grey"];
 const delay = 5000;
 export default function Home() {
@@ -98,6 +99,22 @@ export default function Home() {
                         {isMenuOpen &&
                             <div className="menuHeart">
                                 <p>Favourites</p>
+                                <div className='hamburgerMenuFavourites'>
+                                <img src={"/3 свеча.png"} style={{ width: '15%', height: "15%", borderRadius:'25px' }} alt="" />
+                                <img src={"/4 свеча.png"} style={{ width: '15%', height: "15%", borderRadius:'25px' }} alt="" />
+                                    <div className='favouritesText'>
+                                        <p>CANDLE "WARM"</p>
+                                        <p>CANDLE "PINK SET"</p>
+                                    </div>
+                                    <div className='descriptionOfItems'>
+                                        <p>22$</p>
+                                        <p>19$</p>
+                                        <div className='deleteItemFavourites'>
+                                        <CgCloseO size={27}/>
+                                        <CgCloseO size={27}/>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         }
                         <div className='hamburgerMenuHeart' onClick={hamburgerMenu}>
